7b87080eb633af694daff6658cfe479cbc5b2088
Malek <malekzommit99@gmail.com>
2022-03-31 13:41:21 +0200

    finalcode

--- src/com/mycompany/uis/AddStudent.java
+++ src/com/mycompany/uis/AddStudent.java
@@ -14,6 +14,7 @@ import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
+import com.codename1.ui.animations.CommonTransitions;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
@@ -30,7 +31,10 @@ public class AddStudent extends Form{
     Toolbar tb=getToolbar();
 		setTitle("Ajout des etudiants");
 		Form previous = Display.getInstance().getCurrent();
-		tb.setBackCommand("", e -> previous.showBack());
+		 tb.setBackCommand("", e -> {
+            setTransitionOutAnimator(CommonTransitions.createSlide(CommonTransitions.SLIDE_HORIZONTAL, true, Integer.parseInt("200")));
+            new ListStudents(res).show();
+        });
 		Container cnt = new Container(BoxLayout.y());
                 
                 
@@ -54,7 +58,7 @@ public class AddStudent extends Form{
         
         ajouter.addActionListener((ActionListener) (ActionEvent evt) -> {
             // add a book
-            User stu = new User(username.getText(),prenom.getText() ,email.getText(),password.getText() );
+            User stu = new User(prenom.getText() ,username.getText(),email.getText(),password.getText() );
            // new  ServiceStudent.ajoutStudent(stu);
             new  ServiceStudent().AddStudent(stu);
             new ListStudents(res).show();
--- src/com/mycompany/uis/UpdateAdmin.java
+++ src/com/mycompany/uis/UpdateAdmin.java
@@ -12,6 +12,7 @@ import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
+import com.codename1.ui.animations.CommonTransitions;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.util.Resources;
 import com.mycompany.entities.User;
@@ -33,6 +34,10 @@ public class UpdateAdmin extends Form{
         setToolbar(tb);
         getTitleArea().setUIID("Container");
         setTitle("Modifier un administrateur");
+        	 tb.setBackCommand("", e -> {
+            setTransitionOutAnimator(CommonTransitions.createSlide(CommonTransitions.SLIDE_HORIZONTAL, true, Integer.parseInt("200")));
+            new ListAdmin(res).show();
+        });
         getContentPane().setScrollVisible(false);
         
         
